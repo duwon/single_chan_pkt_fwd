4e581869cd0852052812ae22bc249454f538c4f3
Duwon <duwon@duwon.net>
2019-07-18 22:36:37 +0900

    test

--- lorawan_gateway.py
+++ lorawan_gateway.py
@@ -66,8 +66,7 @@ ttn_server_addr = ttn_server['address']
 
 def select_freq():
     display.fill(0)
-    display.text("Select Frequency", 10, 0, 1)
-    display.text('{0} MHz, SF{1}'.format(gateway_freq, gateway_sf), 15, 10, 1)   
+    display.text("Select Frequency", 10, 0, 1) 
     display.show()
     time.sleep(3) 
 
